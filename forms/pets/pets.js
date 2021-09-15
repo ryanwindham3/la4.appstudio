8785a5e7a003834d97fc4727d515db3c6b634657
ryanwindham3 <90585845+ryanwindham3@users.noreply.github.com>
2021-09-15 15:16:24 -0500

    Initial commit

---
+++ forms/pets/pets.js
@@ -0,0 +1,10 @@
+petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+withSal = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+
+console.log(`The third pet is named ${petNames[2]}`)
+console.log(`The array has ${petNames.length} names in it`)
+
+petNames.pop()
+console.log(petNames)
+
+//hello
